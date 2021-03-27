f6b7092b45433a474b0ace9bf04e86f892417439
Devrath AD <devrathad@mplgaming.com>
2021-03-27 23:16:50 +0530

    x

--- app/src/main/java/com/demo/code/data/RemoteDataSource.kt
+++ app/src/main/java/com/demo/code/data/RemoteDataSource.kt
@@ -1,4 +1,17 @@
 package com.demo.code.data
 
-class RemoteDataSource() {
+import com.demo.code.data.network.FoodRecipesApi
+import com.demo.code.models.FoodRecipe
+import retrofit2.Response
+import javax.inject.Inject
+
+/** Here we have injected remote data source with food recipe api **/
+class RemoteDataSource  @Inject constructor(
+    private val foodRecipesApi: FoodRecipesApi
+) {
+
+    suspend fun getRecipes(queries: Map<String, String>): Response<FoodRecipe> {
+        return foodRecipesApi.getRecipes(queries)
+    }
+
 }
