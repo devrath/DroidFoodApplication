f0a680a66b1777477bcb3e3561437fee784ddbf7
Devrath AD <devrathad@mplgaming.com>
2021-03-27 11:27:31 +0530

    Bottom navigation view

---
+++ app/src/main/java/com/demo/code/fragments/RecipesFragment.kt
@@ -0,0 +1,20 @@
+package com.demo.code.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.demo.code.R
+
+class RecipesFragment  : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_recipes, container, false)
+    }
+
+}
