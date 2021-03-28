370d4a0df120e1bb6107f6889fe68db23ff9b25c
Devrath AD <devrathad@mplgaming.com>
2021-03-28 11:40:00 +0530

    d

--- app/src/main/java/com/demo/code/ui/fragments/RecipesFragment.kt
+++ app/src/main/java/com/demo/code/ui/fragments/RecipesFragment.kt
@@ -4,11 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.demo.code.adapters.RecipesAdapter
 import com.demo.code.databinding.FragmentRecipesBinding
+import com.demo.code.util.NetworkResult
+import com.demo.code.vm.MainViewModel
+import com.demo.code.vm.RecipesViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import org.intellij.lang.annotations.Language
 
 @AndroidEntryPoint
 class RecipesFragment  : Fragment() {
@@ -17,14 +22,15 @@ class RecipesFragment  : Fragment() {
     private var _binding: FragmentRecipesBinding? = null
     private val binding get() = _binding!!
 
-    private lateinit var rvAdapter: RecipesAdapter
-    private lateinit var recepiesList : List<Language>
+    private lateinit var mainViewModel: MainViewModel
+    private lateinit var recipesViewModel: RecipesViewModel
+    private val mAdapter by lazy { RecipesAdapter() }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //mainViewModel = ViewModelProvider(requireActivity()).get(MainViewModel::class.java)
-        //recipesViewModel = ViewModelProvider(requireActivity()).get(RecipesViewModel::class.java)
+        mainViewModel = ViewModelProvider(requireActivity()).get(MainViewModel::class.java)
+        recipesViewModel = ViewModelProvider(requireActivity()).get(RecipesViewModel::class.java)
     }
 
 
@@ -36,7 +42,48 @@ class RecipesFragment  : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupRecyclerView()
+        requestApiData()
+    }
 
+    private fun requestApiData() {
+        mainViewModel.getRecipes(recipesViewModel.applyQueries())
+        mainViewModel.recipesResponse.observe(viewLifecycleOwner, { response ->
+            when(response){
+                is NetworkResult.Success -> {
+                    hideShimmerEffect()
+                    response.data?.let { mAdapter.setData(it) }
+                }
+                is NetworkResult.Error -> {
+                    hideShimmerEffect()
+                    Toast.makeText(
+                        requireContext(),
+                        response.message.toString(),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                is NetworkResult.Loading ->{
+                    showShimmerEffect()
+                }
+            }
+        })
+    }
+
+    private fun setupRecyclerView() {
+        binding.recyclerview.adapter = mAdapter
+        binding.recyclerview.layoutManager = LinearLayoutManager(requireContext())
+        showShimmerEffect()
+    }
+
+    private fun showShimmerEffect() {
+        binding.recyclerview.showShimmer()
+    }
+
+    private fun hideShimmerEffect() {
+        binding.recyclerview.hideShimmer()
+    }
 
 
 }
--- app/src/main/java/com/demo/code/util/Constants.kt
+++ app/src/main/java/com/demo/code/util/Constants.kt
@@ -7,6 +7,14 @@ class Constants {
         const val BASE_URL = "https://api.spoonacular.com"
         const val API_KEY = "5a1e4aee6fd74a2b9d23fd2d14f0706f"
 
+        // API Query Keys
+        const val QUERY_NUMBER = "number"
+        const val QUERY_API_KEY = "apiKey"
+        const val QUERY_TYPE = "type"
+        const val QUERY_DIET = "diet"
+        const val QUERY_ADD_RECIPE_INFORMATION = "addRecipeInformation"
+        const val QUERY_FILL_INGREDIENTS = "fillIngredients"
+
     }
 
 }
