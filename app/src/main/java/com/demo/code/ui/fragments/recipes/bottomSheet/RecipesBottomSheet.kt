b76466eb6c1d163af0893af601a94a5a044018ca
Devrath AD <devrathad@mplgaming.com>
2021-04-03 11:55:14 +0530

    v

--- app/src/main/java/com/demo/code/ui/fragments/recipes/bottomSheet/RecipesBottomSheet.kt
+++ app/src/main/java/com/demo/code/ui/fragments/recipes/bottomSheet/RecipesBottomSheet.kt
@@ -4,11 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.demo.code.databinding.RecipesBottomSheetFragmentBinding
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
-class RecipesBottomSheet : Fragment() {
+class RecipesBottomSheet : BottomSheetDialogFragment() {
 
     companion object {
         fun newInstance() = RecipesBottomSheet()
