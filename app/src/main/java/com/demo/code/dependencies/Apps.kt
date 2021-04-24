ba76e99549f7cc78dd4c6f3c7a8ba33236749a98
Devrath <devrath.dev595@gmail.com>
2021-04-24 23:46:39 +0530

    m

---
+++ app/src/main/java/com/demo/code/dependencies/Apps.kt
@@ -0,0 +1,9 @@
+package com.demo.code.dependencies
+
+object Apps {
+    const val compileSdk = 30
+    const val minSdk = 16
+    const val targetSdk = 29
+    const val versionCode = 1
+    const val versionName = "1.0"
+}
