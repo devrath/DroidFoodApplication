af9f2af2b360d6e69aca3632af6da0b48f0834f5
Devrath <devrath.dev595@gmail.com>
2022-03-13 20:33:30 +0530

    c

--- Applications/MVVMNewsApp-master/app/src/main/java/com/codinginflow/mvvmnewsapp/data/NewsArticleDao.kt
+++ Applications/MVVMNewsApp-master/app/src/main/java/com/codinginflow/mvvmnewsapp/data/NewsArticleDao.kt
@@ -4,8 +4,28 @@ import androidx.paging.PagingSource
 import androidx.room.*
 import kotlinx.coroutines.flow.Flow
 
+/**
+ * OnConflictStrategy.REPLACE : ---> If there is an article with this url as the primary already exists, it will be replaced
+ * suspend : ---> Suspend helps us to pause and resume the code and will shift the work from a UI thread
+ *
+ * FLOW : ---> We get a asynchronous stream of data, So we don't need a suspend modifier
+ *        ---> Also important point to note here is that when the data is updated the flow is automatically emitted
+ *
+ */
+
 @Dao
 interface NewsArticleDao {
 
-    
+    @Query("SELECT * FROM breaking_news INNER JOIN news_articles ON articleUrl = url")
+    fun getAllBreakingNewsArticles() : Flow<List<NewsArticle>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertArticles(articles : List<NewsArticle>)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertBreakingNews(articles : List<BreakingNews>)
+
+    @Query("DELETE FROM breaking_news")
+    suspend fun deleteAllBreakingNews()
+
 }
