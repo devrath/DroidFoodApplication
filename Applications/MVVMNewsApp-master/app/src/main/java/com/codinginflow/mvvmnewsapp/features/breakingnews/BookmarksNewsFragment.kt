fb74132c66e70ef00985648dda14915abdd6dfa2
Devrath <devrath.dev595@gmail.com>
2022-03-13 17:40:09 +0530

    v

---
+++ Applications/MVVMNewsApp-master/app/src/main/java/com/codinginflow/mvvmnewsapp/features/breakingnews/BookmarksNewsFragment.kt
@@ -0,0 +1,8 @@
+package com.codinginflow.mvvmnewsapp.features.breakingnews
+
+import androidx.fragment.app.Fragment
+import com.codinginflow.mvvmnewsapp.R
+
+class BookmarksNewsFragment : Fragment(R.layout.fragment_breaking_news) {
+
+}
