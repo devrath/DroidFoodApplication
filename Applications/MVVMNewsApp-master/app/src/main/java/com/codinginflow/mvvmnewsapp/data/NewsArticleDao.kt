440986fd2f4f46729067155a1a258be3347f9a90
Devrath <devrath.dev595@gmail.com>
2022-03-13 20:14:42 +0530

    mmm

---
+++ Applications/MVVMNewsApp-master/app/src/main/java/com/codinginflow/mvvmnewsapp/data/NewsArticleDao.kt
@@ -0,0 +1,11 @@
+package com.codinginflow.mvvmnewsapp.data
+
+import androidx.paging.PagingSource
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface NewsArticleDao {
+
+    
+}
