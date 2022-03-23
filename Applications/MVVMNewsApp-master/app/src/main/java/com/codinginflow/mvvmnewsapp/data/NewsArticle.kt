9f52e5ca05ddedc594a69fc6d316b76fb2a87f86
Devrath <devrath@mplgaming.com>
2022-03-23 10:00:22 +0530

    v

--- Applications/MVVMNewsApp-master/app/src/main/java/com/codinginflow/mvvmnewsapp/data/NewsArticle.kt
+++ Applications/MVVMNewsApp-master/app/src/main/java/com/codinginflow/mvvmnewsapp/data/NewsArticle.kt
@@ -17,3 +17,15 @@ data class BreakingNews(
     val articleUrl: String,
     @PrimaryKey(autoGenerate = true) val id: Int = 0
 )
+
+/**
+ * We have used a compound primary key here
+ * * In this way if we have the same url 2 times in the search query (Ex: while paginating), The first one will be replaced
+ */
+@Entity(tableName = "search_results", primaryKeys = ["searchQuery", "articleUrl"])
+data class SearchResult(
+    val searchQuery: String,
+    val articleUrl: String,
+    // We manage this number ourself: -> Later we can order result by this query position
+    val queryPosition: Int
+)
